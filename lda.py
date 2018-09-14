59f52f5bb5b300e1835f3457cdebb885cf0290f5
anthooooooooony <reallovexy@gmail.com>
2018-09-14 23:43:28 +1000

    update

--- lda.py
+++ lda.py
@@ -12,7 +12,6 @@ for item in paper_list:
     paper = item['key']
     abstract_list.append(text_processing(paper['abstract']))
     title_list.append(paper['title'])
-    abstract_list.append(paper['abstract'])
     keywords_list.append(paper['keywords'])
 
 # ======================================================================================================================
@@ -26,31 +25,49 @@ tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words='english')
 tf = tf_vectorizer.fit_transform(abstract_list)
 
 # ======================================================================================================================
-# Frequency counting
+# Topic modelling
 #
 
 from sklearn.decomposition import LatentDirichletAllocation
 n_topics = 30
 
-"""
-lda = LatentDirichletAllocation(n_topics=n_topics,
-                                max_iter=50,
-                                learning_method='batch')
+
+lda = LatentDirichletAllocation(
+                                learning_method='online',
+                                doc_topic_prior=0.1,
+                                n_components=30,
+                                perp_tol=0.1,
+                                topic_word_prior=0.2
+                                )
 lda.fit(tf)
-"""
+print lda.perplexity(tf)
+
 
 def print_top_words(model, feature_names, n_top_words):
     for topic_idx, topic in enumerate(model.components_):
-        print "Topic #%d:" % topic_idx
+        print "Topic #{}:".format(topic_idx)
         print " ".join([feature_names[i]
                         for i in topic.argsort()[:-n_top_words - 1:-1]])
 
 
-n_top_words = 20
+def print_doc_topic(model, topic_index):
+    for i in range(topic_index):
+        print 'Document #{}: Topic #{}:'.format(i, model[i].argmax())
+
+
 tf_feature_names = tf_vectorizer.get_feature_names()
-#print_top_words(lda, tf_feature_names, n_top_words)
+print_top_words(lda, tf_feature_names, n_top_words=40)
+print
+print_doc_topic(lda.transform(tf), topic_index=20)
+
+for i in range(10):
+    print abstract_list[i]
 
 
+# ======================================================================================================================
+# Parameter Tuning
+#
+'''
 from sklearn.model_selection import GridSearchCV
 
 parameters = {
@@ -63,6 +80,8 @@ parameters = {
 lda = LatentDirichletAllocation()
 model = GridSearchCV(lda, parameters, verbose=2)
 model.fit(tf)
-
 df = pd.DataFrame.from_dict(model.cv_results_)
 print df.sort_values(by='rank_test_score')
+'''
+
+
