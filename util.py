ab416c56956da784631ccf8917b99e1455179a2f
anthooooooooony <reallovexy@gmail.com>
2018-10-28 20:56:05 +1100

    update

--- util.py
+++ util.py
@@ -1,7 +1,9 @@
 import string
+import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+import couchdb
 
 
 def text_processing(text):
@@ -27,3 +29,46 @@ def text_processing(text):
     #filtered = [ps.stem(w) for w in filtered]
 
     return " ".join(filtered)
+
+
+def normalization():
+
+    server = couchdb.Server('http://admin:11112222@localhost:5984/')
+    db = server['paper_cis_replicate']
+
+    def getPaper():
+
+        l = []
+        paper_list = list(db.view("testing/AllPaper"))
+        for paper in paper_list:
+            l.append(paper['key'])
+        return l
+
+
+    papers = getPaper()
+    for paper in papers:
+        print paper
+        # Remove periods at the end of titles
+        paper['title'] = paper['title'].strip('.')
+
+        # Remove commas in names
+        paper['author(s)'] = [author.replace(',', ' ') for author in paper['author(s)']]
+
+        # Filter out published year
+        regex = '(19|20)\d{2}'
+        for item in paper['date']:
+            if re.match(regex, item):
+                paper['date'] = item
+                break
+
+        for paper2 in papers:
+            if (paper['_id'] != paper2['_id']) and\
+                    (paper['title'].lower() == paper2['title'].lower()):
+                db.delete(paper)
+
+
+    db.update(papers)
+
+
+
+normalization()
